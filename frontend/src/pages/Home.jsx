5769e0677b465c554e985bae1747568c24dc0e64
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-06-04 14:42:08 +0100

    api url change - 2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -23,8 +23,10 @@ function Home() {
   }, [])
 
   // find only featured blogs
-  const filtered = publicBlogs && publicBlogs.filter((item) => item.featured === true)
-
+  const filtered =
+    publicBlogs && Array.isArray(publicBlogs)
+      ? publicBlogs.filter((item) => item.featured === true)
+      : []
   // Programaticly display welcome (name) or welcome for the first time registered
   function greeting() {
     if (user && user.logins < 1) {
@@ -37,43 +39,51 @@ function Home() {
 
   return (
     <>
-      <section className="home-hero">
-        <div className="home-hero-header-container">
-          <div className="hero-head-container">
+      <section className='home-hero'>
+        <div className='home-hero-header-container'>
+          <div className='hero-head-container'>
             <p>{greeting()}</p>
             <h1>creative beginnings</h1>
             <p>your blogging journey</p>
             <p>starts here</p>
-            {!user && <p className="home-info-p">login or register to get started </p>}
+            {!user && (
+              <p className='home-info-p'>login or register to get started </p>
+            )}
           </div>
 
-          <div className="hero-form-container">
+          <div className='hero-form-container'>
             <EmailSignUpForm />
           </div>
 
-          <div className="home-hero-content-container">{/* <p>hello</p> */}</div>
+          <div className='home-hero-content-container'>
+            {/* <p>hello</p> */}
+          </div>
         </div>
       </section>
 
-      <div className="page-container">
-        <div className="featured-blogs-header-container">
+      <div className='page-container'>
+        <div className='featured-blogs-header-container'>
           <h2>
             <span>FEATURED BLOG ARTICLES ON THE GO</span>
           </h2>
           <p>read some of our bloggers featured content </p>
-          <p>from america to new zealand we have an array of interesting reads </p>
+          <p>
+            from america to new zealand we have an array of interesting reads{' '}
+          </p>
           <p>to inspire your next trip </p>
         </div>
         {!publicBlogs && <HomeLoader />}
         {publicBlogs && publicBlogs.length < 1 && (
-          <div className="home-no-blogs-div">
+          <div className='home-no-blogs-div'>
             <h3>no featured blogs yet!</h3>
           </div>
         )}
-        <section className="home-blog-posts">
+        <section className='home-blog-posts'>
           {filtered &&
             filtered.length > 0 &&
-            filtered.map((blog) => <FeaturedBlogItem key={blog._id} blog={blog} />)}
+            filtered.map((blog) => (
+              <FeaturedBlogItem key={blog._id} blog={blog} />
+            ))}
         </section>
       </div>
     </>
